8a492d2b36a587df19fc7d8835677624e33d72a2
Digvijay <digvijaybakare816@gmail.com>
2024-12-04 17:07:49 +0530

    JWT Authentication application with postgresql

---
+++ Newer_JWT_Authontication/src/main/java/com/jwt/NewerJwtAuthonticationApplication.java
@@ -0,0 +1,11 @@
+package com.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewerJwtAuthonticationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NewerJwtAuthonticationApplication.class, args);
+	}
+}
